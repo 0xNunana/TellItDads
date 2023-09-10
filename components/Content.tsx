8f02bbf29bad06ce57dc8a90377f444e2d4e7d24
0xNunana <kudayapaul@gmail.com>
2023-09-10 12:53:20 +0000

    To dos

--- components/Content.tsx
+++ components/Content.tsx
@@ -8,6 +8,61 @@ const Content = () => {
             <div className='w-full relative h-[500px]'>
             <Image src='/logo.jpg' fill alt='bg'/>
             </div>
+            <p className='text-center p-3'>Features to be worked on. Kindly contribute to the work. Contributors will be featured in the footer section</p>
+            <div className='grid sm:grid-cols-3 p-5 gap-4'>
+            <div className='bg-red-100 p-5'>
+  <p>Auhentication</p>
+</div>
+<div className='bg-red-100 p-5'>
+  <p>Suggest a feature</p>
+</div>
+<div className='bg-red-100 p-5'>
+  <p>Blogs</p>
+</div><div className='bg-red-100 p-5'>
+  <p>Mentorship</p>
+</div><div className='bg-red-100 p-5'>
+  <p>Member Spotlight </p>
+</div><div className='bg-red-100 p-5'>
+  <p>Polls</p>
+</div><div className='bg-red-100 p-5'>
+  <p>Privacy</p>
+</div><div className='bg-red-100 p-5'>
+  <p>About Us</p>
+</div>
+<div className='bg-red-100 p-5'>
+  <p>Marketplace</p>
+</div>
+<div className='bg-red-100 p-5'>
+  <p>Admin dasboard</p>
+</div>
+<div className='bg-red-100 p-5'>
+  <p>Funding/wallet</p>
+</div>
+<div className='bg-red-100 p-5'>
+  <p>Investment</p>
+</div>
+<div className='bg-red-100 p-5'>
+  <p>Health</p>
+</div>
+<div className='bg-red-100 p-5'>
+  <p>Event/ Calendar</p>
+</div>
+<div className='bg-red-100 p-5'>
+  <p>Multiple language support</p>
+</div>
+<div className='bg-red-100 p-5'>
+  <p>Partnerships</p>
+</div>
+<div className='bg-red-100 p-5'>
+  <p>Survey and feedback</p>
+</div>
+<div className='bg-red-100 p-5'>
+  <p>Mobile app</p>
+</div>
+<div className='bg-red-100 p-5'>
+  <p>Library</p>
+</div>
+            </div>
 
         </div>
         </div>
