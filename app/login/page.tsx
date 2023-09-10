e899cbf3fb2df0482c6b4e92c309a44dcc08230c
0xNunana <kudayapaul@gmail.com>
2023-09-10 12:25:21 +0000

    login logo route to home

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,14 +1,17 @@
+import Link from 'next/link'
 import React from 'react'
 
 const page = () => {
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
     <div className="sm:mx-auto sm:w-full sm:max-w-md">
-      <img
+  <Link href='/'>
+  <img
         className="mx-auto h-16 w-auto"
         src="/logo.jpg"
         alt="TID LOGO"
       />
+  </Link>
       <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
 Sign in to your account
       </h2>
